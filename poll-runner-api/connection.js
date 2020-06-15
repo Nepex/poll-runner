4de1f3758e8579db1c36e13ee60c3ac9878b0d81
nepex <thechump@hotmail.com>
2020-06-15 00:27:58 -0500

    update packages

--- poll-runner-api/connection.js
+++ poll-runner-api/connection.js
@@ -3,7 +3,7 @@ const Pool = require('pg').Pool
 module.exports = new Pool({
   user: 'postgres',
   host: 'localhost',
-  database: 'poll-runner',
+  database: 'poll_runner',
   password: 'root',
   port: 5432,
 });
