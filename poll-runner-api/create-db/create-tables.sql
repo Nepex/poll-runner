54be37e02193b6bc7dc123230da2b1f2dc92fb66
nepex <thechump@hotmail.com>
2020-06-15 01:48:22 -0500

    alter create-tables

--- poll-runner-api/create-db/create-tables.sql
+++ poll-runner-api/create-db/create-tables.sql
@@ -1,18 +1,18 @@
-CREATE TABLE public.users
+CREATE TABLE poll_runner.public.users
 (
     "id" uuid NOT NULL,
     "first_name" character varying(255) NOT NULL,
     "last_name" character varying(255) NOT NULL,
     "email" character varying(255) NOT NULL,
-    "password" character varying(255),
+    "password" character varying(255) NOT NULL,
     "is_admin" boolean NOT NULL DEFAULT False,
     PRIMARY KEY ("id")
 );
 
-ALTER TABLE public.users
+ALTER TABLE poll_runner.public.users
     OWNER to postgres;
 
-CREATE TABLE public.polls
+CREATE TABLE poll_runner.public.polls
 (
     "id" uuid NOT NULL,
     "poll_name" character varying(255) NOT NULL,
@@ -20,10 +20,10 @@ CREATE TABLE public.polls
     PRIMARY KEY ("id")
 );
 
-ALTER TABLE public.polls
+ALTER TABLE poll_runner.public.polls
     OWNER to postgres;
 
-CREATE TABLE public.active_polls
+CREATE TABLE poll_runner.public.active_polls
 (
     "id" uuid NOT NULL,
     "user_id" uuid NOT NULL,
@@ -34,5 +34,7 @@ CREATE TABLE public.active_polls
     PRIMARY KEY ("id")
 );
 
-ALTER TABLE public.active_polls
+ALTER TABLE poll_runner.public.active_polls
     OWNER to postgres;
+
+INSERT INTO users (id, first_name, last_name, email, password, is_admin) VALUES ('845ba708-c7fe-42d6-bae5-74453a67abac', 'Mister', 'Admin', 'admin@example.com', 'admin', true);
